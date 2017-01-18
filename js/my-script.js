002574dc6045c1102afa2da71afc3415de307373
mikecee <mikecee0815@gmail.com>
2017-01-18 17:45:58 -0500

    gh-pages setup, updated info

--- js/my-script.js
+++ js/my-script.js
@@ -5,9 +5,15 @@ jQuery(document).ready(function($) {
 
 	aboutLeft.html( function(){ 
 
-		return  "Hello,My name is michael.I am a self-taught web Developer/programmer. I am available for website design and development work, currently i am learning the AngularJS and also Bootstrap3 framework. I continue to  grow my skills set daily and my main goal is to be a full stack web programmer.";}
-		);
-	aboutRight.html("<p>Im also a Musician who enjoys playing in public. When im not gigging,im most likely coding a webpage.</p>");
+		return  
+
+		"Hello,My name is michael.I am a self-taught web Developer/programmer. 
+		I am available for website design and development work, 
+		currently i am learning the AngularJS and also Bootstrap3 framework. 
+		I continue to  grow my skills set daily.";});
+
+	aboutRight.html("<p>Im also a Musician who enjoys playing in public. 
+			          When im not gigging,im most likely coding a webpage.</p>");
 
 	$('.location').html('<h3>Long Island, New York</h3>')
 	$('.social-media-links h3').html('Find me on the web')
